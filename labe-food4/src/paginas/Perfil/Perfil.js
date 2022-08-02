614c209e59bd2028c33309a0dfb64fbc623ff56c
Mariotin <gmariotin@gmail.com>
2022-08-02 17:01:30 -0300

    perfil

--- labe-food4/src/paginas/Perfil/Perfil.js
+++ labe-food4/src/paginas/Perfil/Perfil.js
@@ -6,29 +6,36 @@ import { Cabecalho } from "./Style";
 import { DadosPessoal } from "./Style";
 import { PerfilLetras } from "./Style";
 
-const Perfil = () =>{
+const Perfil = () => {
 
-    const  {requests, states} = useContext(GlobalContext)
-    const {PegarPerfil} = requests
-    const {perfil} = states
-    
-    useEffect (() =>{
+    const { requests, states } = useContext(GlobalContext)
+    const { PegarPerfil, PegarHistoricoPedidos } = requests
+    const { perfil, historicoPedidos } = states
+
+    useEffect(() => {
         PegarPerfil()
+        PegarHistoricoPedidos()
     }, [])
 
     return (
-       <Container>
-        <Cabecalho>Meu Perfil</Cabecalho>
-         {perfil ? 
-        <DadosPessoal>
-        <PerfilLetras>{perfil.name}</PerfilLetras>
-        <PerfilLetras>{perfil.email}</PerfilLetras>
-        <PerfilLetras>{ perfil.cpf}</PerfilLetras> 
-        </DadosPessoal> : <p>Carregando...</p>}
+        <Container>
+            <Cabecalho>Meu Perfil</Cabecalho>
+            {perfil ?
+                <DadosPessoal>
+                    <PerfilLetras>{perfil.name}</PerfilLetras>
+                    <PerfilLetras>{perfil.email}</PerfilLetras>
+                    <PerfilLetras>{perfil.cpf}</PerfilLetras>
+                </DadosPessoal> : <p>Carregando...</p>}
 
-        <div>Endereço</div>
-        <div>Historico de Pedidos</div>
-       </Container>
+            <div>
+                <span>Endereço cadastrado</span>
+                {perfil ?
+                    <div>{perfil.address}</div> : <p>Carregando...</p>}
+            </div>
+            <div>Historico de Pedidos</div>
+
+        </Container>
+
     )
 }
 
